3a4052c23bb8a957dfd6137f225f337751103b9a
connorw <connorwbest95@gmail.com>
2018-10-10 23:46:25 -0400

    full functionality on individual class page, minor bugs and issues remaining

--- cobatext/controllers/classController.js
+++ cobatext/controllers/classController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = {
   findByNumber: function(req, res) {
-    db.Class.find({ courseNumber: req.params.number })
+    db.Class.find({ courseNumber: req.body.number })
       .then(function(classes) {
         res.json(classes);
       })
@@ -28,25 +28,28 @@ module.exports = {
         res.json(err);
       });
   },
-  findOne: function(req, res) {
-    db.Class.find({ courseNumber: req.params.number, major: req.params.major })
-      .then(function(classes) {
-        res.json(classes);
+  findById: function(req, res) {
+    db.Class.findById(req.params.id)
+      .then(classes => res.json(classes))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Review.create(req.body)
+      .then(review => res.json(review))
+      .catch(err => res.status(422).json(err));
+  },
+  findReviews: function(req, res) {
+    db.Review.find({ class: req.params.id })
+      .then(function(reviews) {
+        res.json(reviews);
       })
       .catch(function(err) {
         res.json(err);
       });
   },
-  findById: function(req, res) {
-    db.Class
-      .findById(req.params.id)
-      .then(classes => res.json(classes))
+  updateClass: function(req, res) {
+    db.Class.findOneAndUpdate({ _id: req.body.class }, {$push: {"grade": req.body.grade, "purchase": req.body.purchase, "use": req.body.usage}})
+      .then(course => res.json(course))
       .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Review
-      .create(req.body)
-      .then(review => res.json(review))
-      .catch(err => res.status(422).json(err));
-  },
+  }
 };
