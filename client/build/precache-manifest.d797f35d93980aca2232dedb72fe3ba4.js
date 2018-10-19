20bbe9aae29565339dafb0a1332965f4eaa862d1
connorw <connorwbest95@gmail.com>
2018-10-19 00:33:25 -0400

    repo structure

---
+++ client/build/precache-manifest.d797f35d93980aca2232dedb72fe3ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9136bbb15ee16b922918f1071d50641c",
+    "url": "/static/media/black-scribble-png-5.9136bbb1.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d521f8bb0bf91e891369",
+    "url": "/static/js/main.d521f8bb.chunk.js"
+  },
+  {
+    "revision": "af16634a4a170fff3c6b",
+    "url": "/static/js/1.af16634a.chunk.js"
+  },
+  {
+    "revision": "d521f8bb0bf91e891369",
+    "url": "/static/css/main.cc5cdaec.chunk.css"
+  },
+  {
+    "revision": "05396d809a57af2886be171c90217441",
+    "url": "/index.html"
+  }
+];
